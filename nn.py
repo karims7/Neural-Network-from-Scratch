1ae10510d444ff502876ee8bbe702ba84e726439
karims7 <karims7@my.yorku.ca>
2025-04-15 02:10:58 -0400

    updated back prop function and implemented update parameter function

--- nn.py
+++ nn.py
@@ -50,13 +50,22 @@ def back_prop(Z1, A1, Z2, A2, W2, pixels, label):
     m = label.size
     one_hot_label = one_hot(label)
 
-    dZ2 = A2 - one_hot_label
     dW2 = 1 / m * dZ2.dot(A1.T)
-
-    db2 = 1 / m * np.sum(dZ2, 2)
-    dZ1 = W2.T.dot(dZ2) * derivative_ReLU(Z1)
-
     dW1 = 1 / m * dZ1.dot(pixels.T)
+
+    dZ1 = W2.T.dot(dZ2) * derivative_ReLU(Z1)
+    dZ2 = A2 - one_hot_label
+
     db1 = 1 / m * np.sum(dZ1, 2)
+    db2 = 1 / m * np.sum(dZ2, 2)
     
     return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, DW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    W2 = W2 - alpha * DW2
+
+    b1 = b1 - alpha * db1
+    b2 = b2 - alpha * db2
+
+    return W1, b1, W2, b2
