19f9c39033abe80b923bcdf8f1eebcf29e42439f
karims7 <karims7@my.yorku.ca>
2025-04-15 02:05:02 -0400

    started implementing the neural network

--- nn.py
+++ nn.py
@@ -4,16 +4,59 @@ from matplotlib import pyplot as plt
 
 data = pd.read_csv("train.csv")
 
-print(data.head())
+# print(data.head())
 
 data = np.array(data)
 m, n = data.shape
 np.random.shuffle(data) # avoid shuffling 
 
-valid_data = data[0:1000].T
-Y_dev = valid_data[0]
-X_dev = valid_data[1:n]
-
 train_data = data[1000:m].T
-Y_train = train_data[0]
-X_train = train_data[1:n]
+train_label = train_data[0]
+train_pixels = train_data[1:n]
+
+valid_data = data[0:1000].T
+valid_label = valid_data[0]
+valid_pixels = valid_data[1:n]
+
+def init_params():
+    W1 = np.random.rand(10,784)
+    b1 = np.random.rand(10,1)
+    W2 = np.random.rand(10,10)
+    b2 = np.random.rand(10,1)
+    return W1, b1, W2, b2
+
+def softmax(Z):
+    return np.exp(Z) / np.sum(np.exp(Z))
+
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+
+def ReLU(Z): 
+    return np.maximum(0, Z)
+
+def one_hot(label):
+    one_hot_label = np.zeros((label.size, label.max() + 1))
+    one_hot_label[np.arange(label.size), label] = 1
+    one_hot_label = one_hot_label.T
+    return one_hot_label
+
+def derivative_ReLU(Z):
+    return Z > 0
+
+def back_prop(Z1, A1, Z2, A2, W2, pixels, label):
+    m = label.size
+    one_hot_label = one_hot(label)
+
+    dZ2 = A2 - one_hot_label
+    dW2 = 1 / m * dZ2.dot(A1.T)
+
+    db2 = 1 / m * np.sum(dZ2, 2)
+    dZ1 = W2.T.dot(dZ2) * derivative_ReLU(Z1)
+
+    dW1 = 1 / m * dZ1.dot(pixels.T)
+    db1 = 1 / m * np.sum(dZ1, 2)
+    
+    return dW1, db1, dW2, db2
